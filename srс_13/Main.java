ebcf4333c7496414621d1088e0fff0643395f1a7
deketalina772-afk <deketalina772@gmail.com>
2025-11-11 19:41:55 +0200

    Add files via upload

---
+++ srс_13/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public Main() {}
+
+    public static void main(String[] args) {
+        Transport bmw = new Transport(200.5f, 2000.0f, "red", new byte[] {0, 0, 0});
+
+        Transport truck = new Transport(130.5f, 5000.0f, "White");
+
+
+    }
+}
