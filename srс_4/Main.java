360f68ea96469ba4d62776e8357f13885f56067c
deketalina772-afk <deketalina772@gmail.com>
2025-11-11 08:10:26 +0200

    Add files via upload

---
+++ srс_4/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+        Car bmw = new Car(200.5f, 2000.0f, "red", new byte[]{0, 0, 0});
+
+        Truck truck = new Truck(130.5f, 5000.0f, "White", true);
+        truck.setValues(130.5f, 5000.0f, "White", new byte[]{0, 0, 0}, true);
+        System.out.println(truck.getValues());
+
+    }
+}
