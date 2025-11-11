f72fdae5ed585abc6cbd79292326dc89dacbcdd4
deketalina772-afk <deketalina772@gmail.com>
2025-11-11 19:14:44 +0200

    Add files via upload

---
+++ srс_18/Main.java
@@ -0,0 +1,39 @@
+class Main {
+
+    public Main() {}
+
+    public static void main(String[] args) {
+        DB db = new DB();
+
+        final int num =10;
+        //num = 11;
+
+        Person obj1 = new Person();
+        Person obj2 = new Person();
+        Person obj3 = new Person();
+        Person.GetCount();
+        //Person.count = 10;
+        //System.out.println();
+
+        add(5, 7, 6);
+        add(5.5f, 7.3f);
+        add("Hi", "World");
+    }
+
+    public void add(int a, int b) {
+        int res = a + b;
+        System.out.println("Res:" + res);
+    }
+    public static void add (float a, float b) {
+        float res = a + b;
+        System.out.println("Res:" + res);
+    }
+    public static void add (String a, String b) {
+        String res = a + b;
+        System.out.println("Res:" + res);
+    }
+    public static void add (int a, int b, int y) {
+        int res = a + b + y;
+        System.out.println("Res:" + res);
+    }
+}
---
+++ srс_18/Person.java
@@ -0,0 +1,12 @@
+public class Person {
+
+    public static int count = 0;
+    public int health = 100;
+
+    public Person() {
+        count ++;
+    }
+    public static void GetCount() {
+        System.out.println("Count:" + count);
+    }
+}
