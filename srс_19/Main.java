f4eca8f5f1103534dd9458ec99eaa093e0f2616d
deketalina772-afk <deketalina772@gmail.com>
2025-11-11 20:05:19 +0200

    Add files via upload

---
+++ srс_19/Main.java
@@ -0,0 +1,12 @@
+class Main {
+
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+        Car bmv = new Car();
+        bmv.lightInfo();
+        bmv.setLight(true);
+        bmv.lightInfo();
+    }
+}
