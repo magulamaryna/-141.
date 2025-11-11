18451d7b84436577bd27518a420febc218460de7
deketalina772-afk <deketalina772@gmail.com>
2025-11-11 19:36:22 +0200

    Add files via upload

---
+++ srс_15/Transport.java
@@ -0,0 +1,41 @@
+public class Transport {
+    protected float speed;
+    protected float weight;
+    protected String color;
+    protected byte[] coordinates;
+
+    public Transport() {}
+
+    public  Transport(float _speed, float _weight, String _color, byte[] _coordinates) {
+        // System.out.println("Object is created");
+        setValues(_speed, _weight, _color, _coordinates);
+        System.out.println(getValues());
+    }
+
+    public  Transport(float speed, float weight, String color) {
+        this.speed = speed;
+        this.weight = weight;
+        this.color = color;
+        this.coordinates = new byte[0];
+
+    }
+
+    public void setValues(float _speed, float _weight, String _color, byte[] _coordinates) {
+        speed = _speed;
+        weight = _weight;
+        color = _color;
+        coordinates = _coordinates;
+    }
+
+    public String getValues() {
+        String info = "Object speed: " + speed + " Weight: " + weight + " Color: " + color + "\n";
+        if (coordinates.length > 0) {
+            String infoCoordinates = "Coordinates:\n";
+            for (byte el : coordinates) {
+                infoCoordinates += el + "\n";
+            }
+            return info + infoCoordinates;
+        }
+        return info;
+    }
+}
