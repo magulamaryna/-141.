c4250e6af73416f8c285fcc56594483f264a6812
zolotarnina58-ui <zolotarnina58@gmail.com>
2025-11-12 16:21:14 +0200

    Add files via upload

---
+++ PR_13/src/main/java/com/itproger/Main.java
@@ -0,0 +1,13 @@
+package com.itproger;
+
+class Main {
+
+    public Main() {}
+
+    public static void main(String[] args) {
+        Transport bmw = new Transport(200.5f, 2000, "Black", new byte[] { 0, 0, 0 });
+
+        Transport truck = new Transport(130f, 5000, "White");
+
+    }
+}
