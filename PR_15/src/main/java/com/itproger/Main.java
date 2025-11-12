1122e9dd6c90eefb117ca4839b7cef8e23db381e
zolotarnina58-ui <zolotarnina58@gmail.com>
2025-11-12 19:15:46 +0200

    Add files via upload

---
+++ PR_15/src/main/java/com/itproger/Main.java
@@ -0,0 +1,16 @@
+package com.itproger;
+
+class Main {
+
+    public Main() {}
+
+    public static void main(String[] args) {
+        Car bmw = new Car(200.5f, 2000, "Black", new byte[] { 0, 0, 0 });
+        // System.out.println(bmw.getValues());
+
+        Truck truck = new Truck(130f, 5000, "White", true);
+        truck.setValues(130f, 5000, "White", new byte[] { 0, 100, 0 }, true);
+        System.out.println(truck.getValues());
+    }
+}
+
